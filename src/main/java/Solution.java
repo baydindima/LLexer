24615a12d785a2075ccfa0c634bd60c73fbcc20b
baydindima <QCtEes=9,29)vzW>
2016-04-03 20:46:17 +0300

    Parser

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,10 +1,29 @@
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.gui.TreeViewer;
+
+import javax.swing.*;
+import java.util.Arrays;
 
 public class Solution {
+    private static final int height   = 500;
+    private static final int width    = 1000;
+    private static final double scale = 1.;
 
     public static void main(String[] args) {
-        SimpleExpressionParser parser = ParserFactory.getInstance("read x; if y + 1 == x then write y else skip; write x + 2");
-        parser.addParseListener(new ParserListener());
-        parser.program();
+        SimpleExpressionParser parser = ParserFactory.getInstance(args[0]);
+
+        ParseTree tree = parser.statement();
+
+        JFrame frame = new JFrame("Parser tree");
+        JPanel panel = new JPanel();
+        TreeViewer viewer = new TreeViewer(Arrays.asList(parser.getRuleNames()), tree);
+
+        viewer.setScale(scale);
+        panel.add(viewer);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(width, height);
+        frame.setVisible(true);
     }
 
 }
