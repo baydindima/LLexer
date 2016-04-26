d51f49536cd2ea46dc0514afcbb4b030ae8f73d5
baydindima <QCtEes=9,29)vzW>
2016-04-26 13:07:18 +0300

    Lexer improve

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,11 +1,30 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.Token;
+
+import java.util.List;
 
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        SimpleExpressionParser parser = ParserFactory.getInstance(args[0]);
-        parser.addParseListener(new ParserListener());
-        parser.program();
+        String str = args[0];
+        ANTLRInputStream input = new ANTLRInputStream(str);
+        SimpleExpressionLexer lexer = new SimpleExpressionLexer(input);
+        List<? extends Token> allTokens = lexer.getAllTokens();
+        for (Token symbol : allTokens) {
+            int type = symbol.getType();
+            String typeName;
+            if (type > 0) {
+                typeName = SimpleExpressionLexer.ruleNames[type - 1];
+            } else {
+                typeName = "UNDIFINED";
+            }
+            System.out.printf("Text: %10s, Start: %d, End: %d, Type: %s \n",
+                    symbol.getText(),
+                    symbol.getStartIndex(),
+                    symbol.getStopIndex(),
+                    typeName
+            );
+        }
     }
 
 }
